9f5267357ea61a96cc3740b378e415e59e72a7ca
thev1ndu <itsthw9@gmail.com>
2026-04-11 17:48:28 +0530

    chore: logging filter improved to show statusinfo and request duration

--- src/main/java/com/smartcampus/filter/LoggingFilter.java
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -8,6 +8,9 @@ import javax.ws.rs.ext.Provider;
 import java.util.logging.Logger;
 
 /**
+ * Centralised API logging filter.
+ * Logs method, URI, headers on the way in and method, URI, status, duration on
+ * the way out.
  *
  * @author thevinduw
  */
@@ -15,15 +18,41 @@ import java.util.logging.Logger;
 public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+    private static final String START_TIME = "request-start-time";
 
     @Override
     public void filter(ContainerRequestContext req) {
-        LOGGER.info("OUTGOING REQUEST  -> " + req.getMethod() + " " + req.getUriInfo().getRequestUri());
+        // Store the start time so we can calculate duration in the response filter
+        req.setProperty(START_TIME, System.currentTimeMillis());
+
+        String method = req.getMethod();
+        String uri = req.getUriInfo().getRequestUri().toString();
+        String contentType = req.getHeaderString("Content-Type");
+        String accept = req.getHeaderString("Accept");
+
+        LOGGER.info(">>> REQUEST  " + method + " " + uri
+                + " | Content-Type: " + (contentType != null ? contentType : "N/A")
+                + " | Accept: " + (accept != null ? accept : "*/*"));
     }
 
     @Override
     public void filter(ContainerRequestContext req, ContainerResponseContext res) {
-        LOGGER.info("INCOMING RESPONSE <- " + req.getMethod() + " " + req.getUriInfo().getRequestUri()
-                + " | Status: " + res.getStatus());
+        String method = req.getMethod();
+        String uri = req.getUriInfo().getRequestUri().toString();
+        int status = res.getStatus();
+        String statusInfo = res.getStatusInfo() != null
+                ? res.getStatusInfo().getReasonPhrase()
+                : "Unknown";
+
+        // Calculate elapsed time
+        long duration = 0;
+        Object startTime = req.getProperty(START_TIME);
+        if (startTime != null) {
+            duration = System.currentTimeMillis() - (long) startTime;
+        }
+
+        LOGGER.info("<<< RESPONSE " + method + " " + uri
+                + " | Status: " + status + " " + statusInfo
+                + " | Duration: " + duration + "ms");
     }
 }
